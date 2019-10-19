9e3af62dfea887f7629526a70f1ff42213a50501
John Walthour <john.walthour@gmail.com>
2019-10-19 11:09:13 -0400

    Made this store calibration into a python file.  Changed print statements to logger statements.

--- src/stereo_calibrator.py
+++ src/stereo_calibrator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-print('Importing')
+import logging
+logger = logging.getLogger(__name__)
+logger.info('Importing')
 import cv2
 import cv2.aruco
 import numpy as np
@@ -45,7 +47,7 @@ class StereoCalibrator:
         parms.filterByInertia = True
         parms.minInertiaRatio = 0.5
         
-        print("Orig minDistBetweenBlobs: " + str(parms.minDistBetweenBlobs))
+        logger.debug("Orig minDistBetweenBlobs: " + str(parms.minDistBetweenBlobs))
         parms.minDistBetweenBlobs = 5
         parms.blobColor = 0
          
@@ -58,11 +60,11 @@ class StereoCalibrator:
         """
         all_points_in_3space, all_points_in_images = self._find_point_vectors(image_paths)
         if len(all_points_in_3space) > 0:
-            # print("np.array(all_points_in_3space) = " + repr(np.array(all_points_in_3space)))
+            # logger.debug("np.array(all_points_in_3space) = " + repr(np.array(all_points_in_3space)))
             all_points_in_3space = np.array(all_points_in_3space, dtype=np.float32)
-            # print("all_points_in_3space = " + str(all_points_in_3space))
+            # logger.debug("all_points_in_3space = " + str(all_points_in_3space))
             found,cameraMatrix,distCoeffs,rvecs,tvecs = cv2.calibrateCamera(all_points_in_3space, all_points_in_images, self._IMAGE_SIZE, np.array([]), np.array([]))
-            # print("found: " + repr(found) + ",\n cameraMatrix: " + repr(cameraMatrix) + ",\n distCoeffs: " + repr(distCoeffs) + ",\n rvecs: " + repr(rvecs) + ",\n tvecs: " + repr(tvecs))
+            # logger.debug("found: " + repr(found) + ",\n cameraMatrix: " + repr(cameraMatrix) + ",\n distCoeffs: " + repr(distCoeffs) + ",\n rvecs: " + repr(rvecs) + ",\n tvecs: " + repr(tvecs))
         return cameraMatrix,distCoeffs
     
     def _find_point_vectors(self, image_paths):
@@ -70,10 +72,9 @@ class StereoCalibrator:
         all_points_in_3space = []
         for image_path in image_paths:
             img = cv2.imread(image_path)
-            # print("img : " + repr(img))
             points = np.array([[]])
             found,points = cv2.findCirclesGrid(img, self.CAL_PATTERN_DIMS, points, cv2.CALIB_CB_SYMMETRIC_GRID, self._cal_target_dot_det)
-            print(("Found " + str(len(points)) + " cal points in " + image_path) if found else "No cal pattern found in " + image_path)
+            logger.debug(("Found " + str(len(points)) + " cal points in " + image_path) if found else "No cal pattern found in " + image_path)
             if found:
                 all_points_in_images += [points]
                 all_points_in_3space += [self._cal_3space_pattern]
@@ -86,9 +87,23 @@ class StereoCalibrator:
         right_image_paths : list of strings, each of which is a path to an image from the right camera
         pair_image_paths  : list of twoples, of the form ("/path/to/one/left/image", "/path/to/one/right/image"),
         
+        returns : The output from cv2.stereoCalibrate, stored in a dictionary like this:
+        {
+            'minError':minError,
+            'lCameraMatrix':lCameraMatrix,
+            'lDistCoeffs':lDistCoeffs,
+            'rCameraMatrix':rCameraMatrix,
+            'rDistCoeffs':rDistCoeffs,
+            'R':R,
+            'T':T,
+            'E':E,
+            'F':F,
+        }
         """
         # First must calibrate individual cameras
+        logger.info("Computing left camera calibration")
         lCameraMatrix, lDistCoeffs = self.find_single_cam_calibration(left_image_paths)
+        logger.info("Computing right camera calibration")
         rCameraMatrix, rDistCoeffs = self.find_single_cam_calibration(right_image_paths)
         
         # redefine these 
@@ -96,26 +111,42 @@ class StereoCalibrator:
         right_image_paths = [pair[1] for pair in pair_cal_images]
         
         # Find individual dots in all the images
+        logger.info("Finding points in left images from pairs")
         all_points_in_3space, all_points_in_left_images = self._find_point_vectors(left_image_paths)
+        logger.info("Finding points in right images from pairs")
         all_points_in_3space, all_points_in_right_images = self._find_point_vectors(right_image_paths)
         all_points_in_3space = np.array(all_points_in_3space, dtype=np.float32)
-        # print("all_points_in_3space: " + repr(all_points_in_3space))
-        # print("all_points_in_left_images: " + repr(all_points_in_left_images))
-        # print("all_points_in_right_images: " + repr(all_points_in_right_images))
-        # print("self._IMAGE_SIZE: " + repr(self._IMAGE_SIZE))
-        print("len(all_points_in_3space): " + str(len(all_points_in_3space)))
-        print("len(all_points_in_left_images): " + str(len(all_points_in_left_images)))
-        print("len(all_points_in_right_images): " + str(len(all_points_in_right_images)))
-        retval, lCameraMatrix, lDistCoeffs, rCameraMatrix, rDistCoeffs, R, T, E, F = cv2.stereoCalibrate(all_points_in_3space, all_points_in_left_images, all_points_in_right_images, lCameraMatrix, lDistCoeffs, rCameraMatrix, rDistCoeffs, self._IMAGE_SIZE)
-        print("retval: " + repr(retval))
-        print("lCameraMatrix: " + repr(lCameraMatrix))
-        print("lDistCoeffs: " + repr(lDistCoeffs))
-        print("rCameraMatrix: " + repr(rCameraMatrix))
-        print("rDistCoeffs: " + repr(rDistCoeffs))
-        print("R: " + repr(R))
-        print("T: " + repr(T))
-        print("E: " + repr(E))
-        print("F: " + repr(F))
+        # logger.debug("all_points_in_3space: " + repr(all_points_in_3space))
+        # logger.debug("all_points_in_left_images: " + repr(all_points_in_left_images))
+        # logger.debug("all_points_in_right_images: " + repr(all_points_in_right_images))
+        # logger.debug("self._IMAGE_SIZE: " + repr(self._IMAGE_SIZE))
+        logger.debug("len(all_points_in_3space): " + str(len(all_points_in_3space)))
+        logger.debug("len(all_points_in_left_images): " + str(len(all_points_in_left_images)))
+        logger.debug("len(all_points_in_right_images): " + str(len(all_points_in_right_images)))
+        logger.info("Computing stereo calibration")
+        minError, lCameraMatrix, lDistCoeffs, rCameraMatrix, rDistCoeffs, R, T, E, F = cv2.stereoCalibrate(all_points_in_3space, all_points_in_left_images, all_points_in_right_images, lCameraMatrix, lDistCoeffs, rCameraMatrix, rDistCoeffs, self._IMAGE_SIZE)
+        logger.debug("minError: " + repr(minError))
+        logger.debug("lCameraMatrix: " + repr(lCameraMatrix))
+        logger.debug("lDistCoeffs: " + repr(lDistCoeffs))
+        logger.debug("rCameraMatrix: " + repr(rCameraMatrix))
+        logger.debug("rDistCoeffs: " + repr(rDistCoeffs))
+        logger.debug("R: " + repr(R))
+        logger.debug("T: " + repr(T))
+        logger.debug("E: " + repr(E))
+        logger.debug("F: " + repr(F))
+        retDict = {
+            'minError':minError,
+            'lCameraMatrix':lCameraMatrix,
+            'lDistCoeffs':lDistCoeffs,
+            'rCameraMatrix':rCameraMatrix,
+            'rDistCoeffs':rDistCoeffs,
+            'R':R,
+            'T':T,
+            'E':E,
+            'F':F,
+        }
+        
+        return retDict
         
 def mark_dots(infilepath, outfilepath, detector):
     """
@@ -130,6 +161,8 @@ def mark_dots(infilepath, outfilepath, detector):
     cv2.imwrite(outfilepath, annotated)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
     sc = StereoCalibrator();
     cal_img_dir = 'test images/2019-10-18 stereo cal images/'
     left_image_names  = [
@@ -205,8 +238,13 @@ if __name__ == '__main__':
     right_cal_images = [cal_img_dir + img for img in right_image_names]
     pair_cal_images = [(cal_img_dir + pair[0], cal_img_dir + pair[1]) for pair in pair_image_names]
     all_images = left_image_names + right_image_names
-    det = sc.make_detector()
-    for img in all_images:
-        outfile = 'dotted_' + img;
-        mark_dots(cal_img_dir + img, cal_img_dir + outfile, det)
-    sc.find_stereo_pair_calibration(left_cal_images, right_cal_images, pair_cal_images)
+    # det = sc.make_detector()
+    # for img in all_images:
+        # outfile = 'dotted_' + img;
+        # mark_dots(cal_img_dir + img, cal_img_dir + outfile, det)
+    stereo_cal = sc.find_stereo_pair_calibration(left_cal_images, right_cal_images, pair_cal_images)
+    
+    output_fn = 'src/stereo_cal.py'
+    with open(output_fn, 'w+') as outfile:
+        outfile.write('from numpy import array\nstereo_cal = ' + repr(stereo_cal) + '\n')
+    logger.info('Done, saved calibration to ' + output_fn)
