058a6c69e1a8fa5a76024cbf21d1cdaa52dad03c
John Walthour <john.walthour@gmail.com>
2019-10-19 10:07:05 -0400

    Began updating to use the new dataset - doesn't  yet work

--- src/markerfinder.py
+++ src/markerfinder.py
@@ -116,9 +116,23 @@ class Markerfinder:
                 all_points_in_3space += [self._cal_3space_pattern]
         return all_points_in_3space, all_points_in_images
     
-    def find_stereo_pair_calibration(self, left_image_paths, right_image_paths):
+    def find_stereo_pair_calibration(self, left_image_paths, right_image_paths, pair_image_paths):
+        """
+        
+        left_image_paths  : list of strings, each of which is a path to an image from the left camera
+        right_image_paths : list of strings, each of which is a path to an image from the right camera
+        pair_image_paths  : list of twoples, of the form ("/path/to/one/left/image", "/path/to/one/right/image"),
+        
+        """
+        # First must calibrate individual cameras
         lCameraMatrix, lDistCoeffs = mf.find_single_cam_calibration(left_image_paths)
         rCameraMatrix, rDistCoeffs = mf.find_single_cam_calibration(right_image_paths)
+        
+        # redefine these 
+        left_image_paths = [pair[0] for pair in pair_cal_images]
+        right_image_paths = [pair[1] for pair in pair_cal_images]
+        
+        # Find individual dots in all the images
         all_points_in_3space, all_points_in_left_images = self._find_point_vectors(left_image_paths)
         all_points_in_3space, all_points_in_right_images = self._find_point_vectors(right_image_paths)
         all_points_in_3space = np.array(all_points_in_3space, dtype=np.float32)
@@ -202,17 +216,85 @@ def mark_dots(infilepath, outfilepath, detector):
 
 if __name__ == '__main__':
     mf = Markerfinder();
-    cal_img_dir = 'test images/2019-10-13 stereo cal images/'
-    left_image_names  = ['0l.jpg', '1l.jpg', '2l.jpg', '3l.jpg', '4l.jpg', '5l.jpg']
-    right_image_names = ['0r.jpg', '1r.jpg', '2r.jpg', '3r.jpg', '4r.jpg', '5r.jpg']
-    left_cal_images  = [cal_img_dir + fn for fn in left_image_names ]
-    right_cal_images = [cal_img_dir + fn for fn in right_image_names]
+    cal_img_dir = 'test images/2019-10-18 stereo cal images/'
+    left_image_names  = [
+        'left/left-00001.png',
+        'left/left-00002.png',
+        'left/left-00003.png',
+        'left/left-00004.png',
+        'left/left-00005.png',
+        'left/left-00006.png',
+        'left/left-00007.png',
+        # 'left/left-00008.png',
+        # 'left/left-00009.png',
+        'left/left-00010.png',
+        'left/left-00011.png',
+        'left/left-00012.png',
+        'left/left-00013.png',
+        'left/left-00014.png',
+        'left/left-00015.png',
+        'left/left-00019.png',
+        # 'left/left-00020.png',
+        'left/left-00021.png',
+        'left/left-00022.png',
+        'left/left-00023.png',
+        'left/left-00025.png',
+    ]
+    right_image_names = [
+        'right/right-00001.png',
+        'right/right-00002.png',
+        # 'right/right-00003.png',
+        'right/right-00004.png',
+        'right/right-00005.png',
+        'right/right-00006.png',
+        'right/right-00007.png',
+        'right/right-00008.png',
+        'right/right-00009.png',
+        'right/right-00010.png',
+        'right/right-00011.png',
+        'right/right-00012.png',
+        'right/right-00013.png',
+        # 'right/right-00014.png',
+        # 'right/right-00015.png',
+        'right/right-00019.png',
+        'right/right-00020.png',
+        'right/right-00021.png',
+        'right/right-00022.png',
+        'right/right-00023.png',
+        # 'right/right-00025.png',
+    ]
+    pair_image_names = [
+        ('left/left-00001.png','right/right-00001.png'),
+        ('left/left-00002.png','right/right-00002.png'),
+        # ('left/left-00003.png','right/right-00003.png'),
+        ('left/left-00004.png','right/right-00004.png'),
+        ('left/left-00005.png','right/right-00005.png'),
+        ('left/left-00006.png','right/right-00006.png'),
+        ('left/left-00007.png','right/right-00007.png'),
+        # ('left/left-00008.png','right/right-00008.png'),
+        # ('left/left-00009.png','right/right-00009.png'),
+        ('left/left-00010.png','right/right-00010.png'),
+        ('left/left-00011.png','right/right-00011.png'),
+        ('left/left-00012.png','right/right-00012.png'),
+        ('left/left-00013.png','right/right-00013.png'),
+        # ('left/left-00014.png','right/right-00014.png'),
+        # ('left/left-00015.png','right/right-00015.png'),
+        ('left/left-00019.png','right/right-00019.png'),
+        # ('left/left-00020.png','right/right-00020.png'),
+        ('left/left-00021.png','right/right-00021.png'),
+        ('left/left-00022.png','right/right-00022.png'),
+        ('left/left-00023.png','right/right-00023.png'),
+        # ('left/left-00025.png','right/right-00025.png'),
+    ]
+    left_cal_images = [cal_img_dir + img for img in left_image_names]
+    right_cal_images = [cal_img_dir + img for img in right_image_names]
+    pair_cal_images = [(cal_img_dir + pair[0], cal_img_dir + pair[1]) for pair in pair_image_names]
     all_images = left_image_names + right_image_names
     det = make_detector()
     for img in all_images:
         outfile = 'dotted_' + img;
         mark_dots(cal_img_dir + img, cal_img_dir + outfile, det)
-    mf.find_stereo_pair_calibration(left_cal_images, right_cal_images)
+    mf.find_stereo_pair_calibration(left_cal_images, right_cal_images, pair_cal_images)
     # img = cv2.imread('test images/7 markers on a printout.jpg')
     # print('Processing')
     # debug_img = mf.find_markers(img)
